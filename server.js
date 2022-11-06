b882f93252c82db09fb20c2771a1054e833941c3
Fambirachimwe <t.svaku@gmail.com>
2022-11-06 13:24:14 +0200

    added images to cloud

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ import telOneRoutes from './routes/telone.js'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 
 // connection to mongoDB
