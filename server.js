5b4db572357a4d1e7bf845b1197f4fd4f9d344ad
Fambirachimwe <t.svaku@gmail.com>
2022-11-06 13:21:36 +0200

    added images to cloud

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ import path from 'path';
 import lifeAssuranceRoutes from './routes/lifeAssurance.js';
 import telOneRoutes from './routes/telone.js'
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 // connection to mongoDB
 //'mongodb://localhost/Madyozw'
