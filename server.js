029f1b9fe7eded7628970090411beb8d9ca2e051
Fambirachimwe <t.svaku@gmail.com>
2022-11-15 14:46:05 +0200

    added cors 2

--- server.js
+++ server.js
@@ -29,19 +29,20 @@ mongoose.connection.once('open', () => {
 const app = express();
 const PORT = process.env.PORT || 5600;
 
+app.use(cors("*"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors("*"));
+
 app.use(express.static('public'))
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// });
 
 
 //  the homepage route for the dashboard
