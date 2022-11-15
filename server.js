12624882ad90b3079a53cd2bc8e09fb53f718bd1
Fambirachimwe <t.svaku@gmail.com>
2022-11-15 14:43:51 +0200

    added cors

--- server.js
+++ server.js
@@ -35,6 +35,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors("*"));
 app.use(express.static('public'))
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 
 //  the homepage route for the dashboard
 
